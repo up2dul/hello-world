2d6075798ef646bc62c80af13a5006b2e801cfb6
Abdul <malik301002@gmail.com>
2024-05-23 20:53:54 +0700

    feat(constant): some additions

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,15 +1,33 @@
 import type { Path } from '~/router';
+import type { User } from './types';
 
-export const ACCESS_TOKEN = 'a123456'; // fake access token
+// fake access token
+export enum AccessToken {
+  ADMIN = 'a123456',
+  GUEST = 'g123456',
+}
 
-export const USER = {
-  ADMIN: {
-    EMAIL: 'admin@up2dul.dev',
-    PASSWORD: 'admin123',
+export enum Role {
+  ADMIN = 'admin',
+  GUEST = 'guest',
+}
+
+export const USER: Record<Role, User> = {
+  [Role.ADMIN]: {
+    role: Role.ADMIN,
+    email: 'admin@up2dul.dev',
+    password: 'admin123',
+    name: 'Abdul',
+    imgUrl: 'https://api.dicebear.com/8.x/thumbs/svg?seed=Misty',
+    accessToken: AccessToken.ADMIN,
   },
-  GUEST: {
-    EMAIL: 'guest@up2dul.dev',
-    PASSWORD: 'guest123',
+  [Role.GUEST]: {
+    role: Role.GUEST,
+    email: 'guest@up2dul.dev',
+    password: 'guest123',
+    name: 'Malik',
+    imgUrl: 'https://api.dicebear.com/8.x/thumbs/svg?seed=Casper',
+    accessToken: AccessToken.GUEST,
   },
 };
 
