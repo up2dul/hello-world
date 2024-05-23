a1dd3467ef3255234d3819266e051bb850289bb3
Abdul <malik301002@gmail.com>
2024-05-24 00:58:37 +0700

    feat(pages,comps): create and add overview card with hard coded content

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './menu-nav';
 export * from './user-nav';
+export * from './overview-card';
 export * from './theme-button';
--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,11 +1,17 @@
+import { OverviewCard } from '~/components/common';
 import { Dashboard } from '~/components/layout';
 
 export default function Overview() {
   return (
     <Dashboard>
-      <div className="p-4">
-        <h1>Overview</h1>
-      </div>
+      <h1 className="text-2xl">Overview</h1>
+
+      <section className="mt-8 grid grid-cols-4 gap-8 text-center">
+        <OverviewCard title="Unresolved" content="60" />
+        <OverviewCard title="Overdue" content="16" />
+        <OverviewCard title="Open" content="43" />
+        <OverviewCard title="On Hold" content="64" />
+      </section>
     </Dashboard>
   );
 }
