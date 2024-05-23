ebc0b619b8353e9cbb2e587320b91ab3164b6dc7
Abdul <malik301002@gmail.com>
2024-05-24 06:59:45 +0700

    fix(hooks): bug on toggling theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useTheme() {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('theme') as 'light' | 'dark';
+    if (currentTheme) {
+      setTheme(currentTheme);
+      document.body.classList.add(currentTheme);
+      localStorage.setItem('theme', currentTheme);
+    }
+  }, []);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-    document.body.classList.toggle('dark');
-    localStorage.setItem('theme', theme);
+    setTheme(prevTheme => {
+      const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+      document.body.classList.add(newTheme);
+      document.body.classList.remove(prevTheme);
+      localStorage.setItem('theme', newTheme);
+      return newTheme;
+    });
   };
 
   return {
