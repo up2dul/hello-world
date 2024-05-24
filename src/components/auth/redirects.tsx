ee8fda6e56005eb5823730dd0ed8472f9f8ee72d
Abdul <malik301002@gmail.com>
2024-05-24 09:26:07 +0700

    refactor: remove index path and page

--- src/components/auth/redirects.tsx
+++ src/components/auth/redirects.tsx
@@ -1,19 +1,25 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-import { getIsLoggedIn } from '~/lib/auth';
+import { getIsLoggedIn, getUserData } from '~/lib/auth';
 import { PATH } from '~/lib/constant';
-import type { Path } from '~/router';
+import { Navigate, type Path } from '~/router';
 
 export const Redirects = ({ children }: { children: React.ReactNode }) => {
   const { pathname } = useLocation();
 
   const isLoggedIn = getIsLoggedIn();
+  const user = getUserData();
+  const isPathIndex = pathname === '/';
 
-  const authenticated = isLoggedIn && PATH.PUBLIC.includes(pathname as Path);
+  const authenticated =
+    (isLoggedIn && PATH.PUBLIC.includes(pathname as Path)) || isPathIndex;
   const unauthenticated =
-    !isLoggedIn && PATH.PRIVATE.includes(pathname as Path);
+    (!isLoggedIn && PATH.PRIVATE.includes(pathname as Path)) || isPathIndex;
 
-  if (authenticated) return <Navigate to="/" replace />;
+  if (authenticated) {
+    if (user?.role === 'admin') return <Navigate to="/overview" replace />;
+    return <Navigate to="/tickets" replace />;
+  }
   if (unauthenticated) return <Navigate to="/login" replace />;
 
   return children;
